3d0d56d9285cf76e5875e7a78eba793ef0f7309c
Junki Yamada <geek.junkey@gmail.com>
2026-02-15 19:05:01 +0900

    APIの土台を追加。

---
+++ api/src/main/kotlin/jp/glory/practice/agentic/AgenticCodingAppApplication.kt
@@ -0,0 +1,11 @@
+package jp.glory.practice.agentic
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class AgenticCodingAppApplication
+
+fun main(args: Array<String>) {
+    runApplication<AgenticCodingAppApplication>(*args)
+}
---
+++ api/src/test/kotlin/jp/glory/practice/agentic/AgenticCodingAppApplicationTests.kt
@@ -0,0 +1,11 @@
+package jp.glory.practice.agentic
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class AgenticCodingAppApplicationTests {
+    @Test
+    fun contextLoads() {
+    }
+}
